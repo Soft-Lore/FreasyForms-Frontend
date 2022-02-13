e94b7714b947e4401218da4c2abb432cc343e530
Steven Rocha <67056904+RochaMoran@users.noreply.github.com>
2022-02-13 07:12:09 -0600

    ADD: page about-us and component navbar

---
+++ src/components/organisms/NavBar.jsx
@@ -0,0 +1,92 @@
+import React from 'react'
+import menu_1 from '../../assets/menu-tres-puntos.svg'
+import menu_2 from '../../assets/menu-tres-rayas.svg'
+import profile from '../../assets/snk-titan-steven.png'
+import logo from '../../assets/logo.jpg'
+import { Link } from 'react-router-dom'
+
+export default function NavBar({menu, setMenu}) {
+    const token = 'null'
+
+  return (
+    <nav className="navbar">
+        <Link to="/">
+            <img className='navbar-logo' src={logo} alt="logo" />
+        </Link>
+        <ul>
+            <li>
+                <a className='paragraph' href="#" target="_blank">Formularios</a>
+            </li>
+            {
+                token ? (
+                    <>
+                        <li>
+                            <a className='paragraph' href="#" target="_blank">Mis Formularios</a>
+                        </li>
+                        <li className="dropdown" onClick={() => setMenu(value => !value)}>
+                            <img className="dropbtn" src={menu_1} alt="menu-tres-puntos" />
+                            <div className={menu ? "dropdown-content active-dropdown" : "dropdown-content"} data-dropdown-menu>
+                                <a className='paragraph' href="#">Crear Formulario</a>
+                                <a className='paragraph' href="#">Editar Perfil</a>
+                                <a className='paragraph' href="#">No se que mas</a>
+                            </div>
+                        </li>
+                        <li className="img-profile">
+                            <div className="img-container">
+                                <img src={profile} alt="profile-img" />
+                            </div>
+                        </li>
+                    </>
+                ) : (
+                    <li>
+                        <a className='paragraph' className="start-btn" href="../pages/SignUp.html">Comenzar</a>
+                    </li>
+                )
+            }
+        </ul>
+        <div className="dropdown-menu" onClick={() => setMenu(value => !value)}>
+            <img className="dropbtn-right" src={menu_2} alt="menu-tres-rayas" data-dropdown-button />
+            <ul className={menu ? "active-dropdown-menu" : ''}>
+                {
+                    token ? (
+                        <>
+                         <li className="img-profile-right">
+                            <div className="img-right-container">
+                                <img src={profile} alt="img-profile" />
+                            </div>
+                        </li>
+                        <li className="username">
+                            <span>@BaBuni69</span>
+                        </li>
+                        <li>
+                            <a className='paragraph' href="#">Formularios</a>
+                        </li>
+                        <li>
+                            <a className='paragraph' href="#">Mis formularios</a>
+                        </li>
+                        <li>
+                            <a className='paragraph' href="#">Crear formualrio</a>
+                        </li>
+                        <li>
+                            <a className='paragraph' href="#">Editar perfil</a>
+                        </li>
+                        <li>
+                            <a className='paragraph' href="#">No se que mas</a>
+                        </li>
+                        </>
+                    ) : (
+                        <>
+                            <li>
+                                <a className='paragraph' href="#" target="_blank">Formularios</a>
+                            </li>
+                            <li>
+                                <a className='paragraph' className="start-btn" href="../pages/SignUp.html">Comenzar</a>
+                            </li>
+                        </>
+                    )
+                }
+            </ul>
+        </div>
+    </nav>
+  )
+}
--- src/components/pages/AboutUs.jsx
+++ src/components/pages/AboutUs.jsx
@@ -1,5 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Footer, NavBar } from '../organisms/index'
 
 export default function AboutUs() {
-  return <div>AboutUs</div>;
+  const [menu, setMenu] = useState(false);
+
+  return (
+    <>
+      <NavBar menu={menu} setMenu={setMenu} />
+      <section className="welcome-container">
+          <div className="welcome-info__content">
+              <div className="welcome-info">
+                  <h1 className="title welcome-info__title">
+                      Encuestas y Formularios
+                  </h1>
+                  <p className="paragraph welcome-info__description">
+                      Cree formularios para encuestas sin costo facilmente. 
+                      Envielos en modo privado o publiquelos, y analice los 
+                      datos reunidos.
+                  </p>
+              </div>
+              <div className="welcome-img">
+                  <img src="" alt="welcome-img" />
+              </div>
+          </div>
+          <div className="welcome-btn__start">
+              <a className="paragraph start-btn" href="../pages/SignUp.html">Comience Ahora</a>
+          </div>
+      </section>
+      <section className="block-info__container">
+          <div className="example-img">
+              <img src="" alt="welcome-img" />
+          </div>
+          <div className="block-info">
+              <h2 className="title block-info__title">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </h2>
+              <p className="paragraph block-info__description">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, consequuntur alias. Quasi sequi fugit numquam soluta. Recusandae sint repellat in.
+              </p>
+          </div>
+      </section>
+      <section className="block-info__container">
+          <div className="block-info">
+              <h2 className="title block-info__title">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </h2>
+              <p className="paragraph block-info__description">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, consequuntur alias. Quasi sequi fugit numquam soluta. Recusandae sint repellat in.
+              </p>
+          </div>
+          <div className="example-img">
+              <img src="" alt="welcome-img" />
+          </div>
+      </section>
+      <section className="block-info__container">
+          <div className="example-img">
+              <img src="" alt="welcome-img" />
+          </div>
+          <div className="block-info">
+              <h2 className="title block-info__title">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </h2>
+              <p className="paragraph block-info__description">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, consequuntur alias. Quasi sequi fugit numquam soluta. Recusandae sint repellat in.
+              </p>
+          </div>
+      </section>
+      <Footer />
+  </>
+  )
 }
